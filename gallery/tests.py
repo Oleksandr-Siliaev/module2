33f42cc4940746fb1c0de0b50f7a2d129ab52bcc
Oleksandr-Siliaev <115369769+Oleksandr-Siliaev@users.noreply.github.com>
2023-05-23 11:55:40 +0300

    Update tests.py

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,24 +1,24 @@
 from django.test import TestCase
+from gallery.models import Category, Image
 
 
-from .models import Image, Category
+class CategoryModelTest(TestCase):
+    def test_category_creation(self):
+        category = Category.objects.create(name='Test Category')
+        self.assertEqual(category.name, 'Test Category')
+
 
 class ImageModelTest(TestCase):
-    def setUp(self):
-        self.category = Category.objects.create(name='Test Category')
-        self.image = Image.objects.create(
-            title='Test Image',
-            image='path/to/image.jpg',
-            created_date='2023-05-23',
-            age_limit=18
-        )
-        self.image.categories.add(self.category)
+    def test_image_creation(self):
+        category = Category.objects.create(name='Test Category')
+        image = Image.objects.create(
+            title='Test Image', image='test.jpg',
+            created_date='2023-05-23', age_limit=18)
+        image.categories.add(category)
 
-    def test_image_has_category(self):
-        self.assertEqual(self.image.categories.count(), 1)
-        self.assertEqual(self.image.categories.first(), self.category)
-      
-
-    def test_image_str_representation(self):
-        self.assertEqual(str(self.image), 'Test Image')
-
+        self.assertEqual(image.title, 'Test Image')
+        self.assertEqual(image.image, 'test.jpg')
+        self.assertEqual(image.created_date, '2023-05-23')
+        self.assertEqual(image.age_limit, 18)
+        self.assertEqual(image.categories.count(), 1)
+        self.assertIn(category, image.categories.all())
