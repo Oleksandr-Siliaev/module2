78163824de6a4816ba7f6c78a870827b9d17ca6c
Oleksandr-Siliaev <115369769+Oleksandr-Siliaev@users.noreply.github.com>
2023-05-23 11:08:13 +0300

    Update models.py

--- gallery/models.py
+++ gallery/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+
+
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
+    
+    
 class Image(models.Model):
     title = models.CharField(max_length=255)
     image = models.ImageField(upload_to='images/')
