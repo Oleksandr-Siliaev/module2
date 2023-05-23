2433852349343c7660b0a4b8559bf833aa6f1686
Oleksandr-Siliaev <115369769+Oleksandr-Siliaev@users.noreply.github.com>
2023-05-23 11:55:15 +0300

    Update models.py

--- gallery/models.py
+++ gallery/models.py
@@ -6,8 +6,8 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
-    
-    
+
+
 class Image(models.Model):
     title = models.CharField(max_length=255)
     image = models.ImageField(upload_to='images/')
