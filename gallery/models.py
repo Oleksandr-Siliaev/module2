607349bf4e65c906f3946b4c7b268e465eb43a14
Oleksandr <tortikteo@gmail.com>
2023-05-23 10:08:19 +0300

    h

--- gallery/models.py
+++ gallery/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+class Image(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images/')
+    categories = models.ManyToManyField(Category)
+    created_date = models.DateField()
+    age_limit = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import Image, Category
 
-# Create your tests here.
+class ImageModelTest(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name='Test Category')
+        self.image = Image.objects.create(
+            title='Test Image',
+            image='path/to/image.jpg',
+            created_date='2023-05-23',
+            age_limit=18
+        )
+        self.image.categories.add(self.category)
+
+    def test_image_has_category(self):
+        self.assertEqual(self.image.categories.count(), 1)
+        self.assertEqual(self.image.categories.first(), self.category)
+
+    def test_image_str_representation(self):
+        self.assertEqual(str(self.image), 'Test Image')
+
